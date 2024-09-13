ce982741903f0093f0f414b629d75617e9a67518
Lancerokobe <141099012+Lancerokobe@users.noreply.github.com>
2024-09-13 02:45:31 +0000

    All Commented

--- Tasksheet129.java
+++ Tasksheet129.java
@@ -1,1 +1,39 @@
+interface Animal {
+    boolean feed(boolean timeToEat);
+    void groom();
+    void pet();
+}
 
+class Gorilla implements Animal {
+
+    @Override
+    public boolean feed(boolean timeToEat) {
+        if (timeToEat) {
+            System.out.println("Put gorilla food into cage.");
+            return true;
+        } else {
+            System.out.println("Not the time to feed the gorilla.");
+            return false;
+        }
+    }
+
+    @Override
+    public void groom() {
+        System.out.println("Lather, rinse, repeat.");
+    }
+
+    @Override
+    public void pet() {
+        System.out.println("Pet at your own risk!");
+    }
+}
+
+public class Tasksheet129 {
+    public static void main(String[] args) {
+        Gorilla gorilla = new Gorilla();
+        
+        gorilla.feed(true);
+        gorilla.groom();
+        gorilla.pet();
+    }
+}
